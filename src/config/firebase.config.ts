4f50aa5b27901422e563411281f3c0dabb4a6f6c
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-02-10 18:37:37 +0300

    ♻️ Remove commented code

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -12,12 +12,3 @@ const firebaseApp = initializeApp({
 });
 
 export const db = getFirestore(firebaseApp);
-
-// const firebaseConfig = {
-//     apiKey: process.env.API_KEY,
-//     authDomain: process.env.AUTH_DOMAIN,
-//     projectId: process.env.PROJECT_ID,
-//     storageBucket: process.env.STORAGE_BUCKET,
-//     messagingSenderId: process.env.MESSAGING_SENDER_ID,
-//     appId: process.env.APP_ID,
-// };
